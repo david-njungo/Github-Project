fb36b1b5b901b41d500986ece0b01ee1a8166c97
David Njungo <davidnjungo3@gmail.com>
2021-08-10 05:35:18 +0300

    add repo request and promise

--- Github-Project/src/app/repo.service.ts
+++ Github-Project/src/app/repo.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../environments/environment';
 
+import { Repository } from './repository';
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
-
-  constructor() { }
+repo!:Repository
+  constructor(private http:HttpClient) { 
+    this.repo = new Repository("", "","",)
+  }
+  repoRequest(){
+    interface ApiResponse{
+      name:string;
+      description:string;
+      url:string;
+  }
+  let promise = new Promise<void>((resolve,reject)=>{
+    this.http.get<ApiResponse>(environment.reposurl).toPromise().then(response=>{
+      this.repo.name = response.name
+      this.repo.description = response.description
+      this.repo.url = response.url
+      resolve()
+    },
+    error=>{
+      reject(error)
+    })
+  })
+  return promise
+}
 }
